7bbea5c7fca5e0e7c44c1f46e726eadfa9338f28
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-08-22 20:57:27 +0200

    build: upgrade dagger and jwt-simple
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,36 +1,31 @@
-{
-  pkgs,
-  inputs,
-  ...
-}:
+{ pkgs, inputs, ... }:
 
 {
   cachix.pull = [ "sagikazarmark-dev" ];
 
-  dotenv.enable = true;
+  # dotenv.enable = true;
 
   dagger.enable = true;
+  env.DAGGER_X_RELEASE = "v1.0.0-beta.10";
 
-  env = {
-    CC_wasm32_unknown_unknown = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
-  };
+  env.CC_wasm32_unknown_unknown = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
 
-  overlays = [
-    (
-      final: prev:
-      let
-        unstable = inputs.nixpkgsUnstable.legacyPackages.${prev.stdenv.hostPlatform.system};
-      in
-      {
-        inherit (unstable)
-          chromedriver
-          geckodriver
-          wasm-pack
-          wasm-bindgen-cli_0_2_108
-          ;
-      }
-    )
-  ];
+  # overlays = [
+  #   (
+  #     final: prev:
+  #     let
+  #       unstable = inputs.nixpkgsUnstable.legacyPackages.${prev.stdenv.hostPlatform.system};
+  #     in
+  #     {
+  #       inherit (unstable)
+  #         chromedriver
+  #         geckodriver
+  #         wasm-pack
+  #         wasm-bindgen-cli_0_2_108
+  #         ;
+  #     }
+  #   )
+  # ];
 
   packages = with pkgs; [
     cargo-audit
@@ -38,7 +33,8 @@
     cargo-watch
     cargo-expand
     wasm-pack
-    wasm-bindgen-cli_0_2_108
+    # wasm-bindgen-cli_0_2_108
+    wasm-bindgen-cli_0_2_126
     # chromedriver
     geckodriver
   ];
